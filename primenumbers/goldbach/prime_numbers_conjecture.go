657424ae61bc7993182c425ee1fd0577242459a3
Emil Georgiev <emogeorgiev88@gmail.com>
2023-02-19 21:35:53 +0200

    add comments for RepresentEvenNumAsSumOfTwoPrimeNums function

--- primenumbers/goldbach/prime_numbers_conjecture.go
+++ primenumbers/goldbach/prime_numbers_conjecture.go
@@ -2,21 +2,29 @@ package goldbach
 
 import "github.com/EmilGeorgiev/algorithms/primenumbers"
 
-// RepresentEvenNumAsSumOfTwoPrimeNums is evidence that each even
-// number greater than 2 can be represented as a  sum of two prime numbers.
+// RepresentEvenNumAsSumOfTwoPrimeNums takes an even number as input and returns
+// a slice of pairs of prime numbers that add up to the given even number.
 func RepresentEvenNumAsSumOfTwoPrimeNums(evenNumber uint64) [][2]uint64 {
 	var result [][2]uint64
 
+	// Start with the largest possible prime number less than the even number and
+	// check if the difference between the even number and that prime number is also prime.
+	// If it is, add the pair of prime numbers to the result slice.
 	for i := evenNumber - 1; i >= 2; i-- {
 		if primenumbers.IsPrime(i) {
 			if primenumbers.IsPrime(evenNumber - i) {
 				result = append(result, [2]uint64{evenNumber - i, i})
 			}
 		}
+
+		// Once 'i' is less than or equal to evenNumber-i, all possible pairs of prime numbers
+		// have been checked, and we can return the result.
 		if i <= evenNumber-i {
 			return result
 		}
 	}
 
+	// If no pairs of prime numbers that add up to the given even number were found,
+	// an empty slice is returned.
 	return result
 }
